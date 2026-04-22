1c09aa1118d59d5ae8099d3111ba5186a7c1cb2d
Aashika <khatiwadaaashika12@gmail.com>
2026-04-22 18:24:30 +0545

    added contacts page

--- Hello/home/urls.py
+++ Hello/home/urls.py
@@ -4,8 +4,9 @@ from home import views
 
 urlpatterns = [
     path('', views.index,name='home'),
-    path('about',views.about,name='about')
-    path('services',views.services,name='serviices')
+    path('about',views.about,name='about'),
+    path('services',views.services,name='serviices'),
+    path('contacts',views.contacts,name='contacts'),
 
 
 ]
--- Hello/home/views.py
+++ Hello/home/views.py
@@ -9,3 +9,6 @@ def about(request):
 
 def services(request):
     return HttpResponse('This is services page')
+
+def contacts(request):
+    return HttpResponse('This is contacts page')
