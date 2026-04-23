d7cdd643ab2e8d7550e5e307eaf4617b113f489e
Aashika <khatiwadaaashika12@gmail.com>
2026-04-23 13:44:36 +0545

    context parameter of render

--- Hello/home/views.py
+++ Hello/home/views.py
@@ -3,8 +3,8 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 def index(request):
     # return HttpResponse("This is homepage.")
-    context={'name':"Aashika",'age':22,'salary':4500000}
-    return render(request,'index.html',context)
+    context={'name':"Aashika",'age':22,'salary':4500000} #dict but it sends...
+    return render(request,'index.html',context)   #....individual keys in html like name age and salary from render
 
 def about(request):
     return HttpResponse("This is about page")
