83f26fb0a83b05bf1113652150b69bb9c27712bd
Aashika <khatiwadaaashika12@gmail.com>
2026-04-23 13:35:23 +0545

    context parameter of render

--- Hello/home/views.py
+++ Hello/home/views.py
@@ -3,7 +3,8 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 def index(request):
     # return HttpResponse("This is homepage.")
-    return render(request,'index.html',{'name':"Aashika"})
+    return render(request,'index.html',{'name':"Aashika",
+                                        'age':22,'salary':4500000})
 
 def about(request):
     return HttpResponse("This is about page")
