2a024ad3fb118fa4bd2d3ce9140d6a05fdf64f53
Aashika <khatiwadaaashika12@gmail.com>
2026-04-26 10:35:11 +0545

    django

--- Hello/home/urls.py
+++ Hello/home/urls.py
@@ -5,7 +5,7 @@ from home import views
 urlpatterns = [
     path('', views.index,name='home'),
     path('about',views.about,name='about'),
-    path('services',views.services,name='serviices'),
+    path('services',views.services,name='services'),
     path('contacts',views.contacts,name='contacts'),
 
 
