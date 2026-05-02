83717b52640b2bb4d45b10b1b157f858be4d774d
Aashika <khatiwadaaashika12@gmail.com>
2026-05-02 20:51:31 +0545

    added block titles

--- Hello/home/views.py
+++ Hello/home/views.py
@@ -18,7 +18,8 @@ def contacts(request):
     return render(request,'contact.html')
     # return HttpResponse('This is contacts page')
 def web(request):
-    return HttpResponse('This is web page of services')
+    # return HttpResponse('This is web page of services')
+    return render(request,'services-web.html')
 def app(request):
     return HttpResponse('This is app page of services')
 def design(request):
