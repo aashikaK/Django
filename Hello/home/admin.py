68457bf760025ebb5da19e2f927c03a0d54ecf77
Aashika <khatiwadaaashika12@gmail.com>
2026-05-02 20:10:48 +0545

    creating base

--- Hello/home/admin.py
+++ Hello/home/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+
+admin.site.site_header = "Aashika Admin Panel"
+admin.site.site_title = "Aashika Dashboard"
+admin.site.index_title = "Welcome to Aashika's Control Panel"
 # Register your models here.
