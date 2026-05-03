24d56857a077785a020c83a48fbaeae5efb82a54
Aashika <khatiwadaaashika12@gmail.com>
2026-05-03 11:13:15 +0545

    adding functionalities

--- Hello/home/views.py
+++ Hello/home/views.py
@@ -21,6 +21,8 @@ def web(request):
     # return HttpResponse('This is web page of services')
     return render(request,'services-web.html')
 def app(request):
-    return HttpResponse('This is app page of services')
+    # return HttpResponse('This is app page of services')
+    return render(request,'app.html')
 def design(request):
-    return HttpResponse('This is design page of services')
+    # return HttpResponse('This is design page of services')
+    return render(request,'uiux.html')
