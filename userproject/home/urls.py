1fae41644f05589ef50cdbe3c9f2810b15bc4df9
Aashika <khatiwadaaashika12@gmail.com>
2026-05-07 20:08:13 +0545

    added url

---
+++ userproject/home/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path,include
+
+urlpatterns = [
+    # path('admin/', admin.site.urls),
+    path('', views.index, name="home"),
+    path('login', views.login, name="login"),
+    path('logout', views.login, name="logout"),
+
+]
