d95f5a59e0c818c79944a114b28f3474aef07a13
Aashika <khatiwadaaashika12@gmail.com>
2026-05-07 20:19:16 +0545

    added functions in view

--- userproject/home/views.py
+++ userproject/home/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
 # Create your views here.
 def index(request):
-    render(request,"index.html")
+    return render(request,"index.html")
+def login(request):
+    return render(request,"login.html")
+def logout(request):
+    return render(request,"login.html")
