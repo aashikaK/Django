032357c72025fef55253dae28be793565f3a8777
Aashika <khatiwadaaashika12@gmail.com>
2026-05-08 20:30:12 +0545

    authentiation

--- userproject/home/views.py
+++ userproject/home/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render,redirect
-from django.contrib.auth.models import Users
-
+from django.contrib.auth.models import user
+from django.contrib.auth import authenticate,logout
 
 # pw for aashi is Aashika123
 # Create your views here.
 def index(request):
-    return render(request,"index.html")
+    if request.user.is_anonymous:
+        return render(request, "login.html")
+
+    else:
+        return render(request,"index.html")
 
 def login(request):
     if request.method=="POST":
