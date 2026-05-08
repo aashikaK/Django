8c88483629a8a1826f069b234a50a2157b808030
Aashika <khatiwadaaashika12@gmail.com>
2026-05-08 20:44:18 +0545

    index.html

--- userproject/home/views.py
+++ userproject/home/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render,redirect
-from django.contrib.auth.models import user
+from django.contrib.auth.models import User
 from django.contrib.auth import authenticate,logout
 
 # pw for aashi is Aashika123
