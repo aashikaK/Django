2621d660f14bd959a725b862d95ef3397558d28c
Aashika <khatiwadaaashika12@gmail.com>
2026-05-08 19:49:44 +0545

    authentiation

--- userproject/home/views.py
+++ userproject/home/views.py
@@ -9,6 +9,10 @@ def index(request):
 
 def login(request):
     if request.method=="POST":
+        username=request.POST.get('username')
+        password=request.POST.get('password')
+
+        user= authenticate(username=username,password=password)
 
     return render(request,"login.html")
 
